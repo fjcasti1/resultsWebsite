0c7a00ea57ccb7c348098f1e21b619a1301a09f2
Francisco Castillo <fjcasti1@asu.edu>
2020-08-25 19:25:05 -0700

    Rename directories

--- update.sh
+++ update.sh
@@ -2,23 +2,22 @@
 # Place in home directory #
 ###########################
 
-# Update Finite Difference Pages
-#cd public_html/resultsWebsite/knifeEdge2DFD
-#source gen_monitors.sh
-#source gen_movies.sh
-cd ~
-# Update Spectral Axisymmetric Pages
-cd public_html/resultsWebsite/spectral3DAxisym/
-cd public_html/resultsWebsite/knifeEdgeSpectral3DAxisym
+# Update Knife Edge Finite Difference Pages
+cd ~/public_html/resultsWebsite/knifeEdgeFD2D
+source gen_monitors.sh
+source gen_movies.sh
+## Update Knife Edge Spectral 3D Pages
+cd ~/public_html/resultsWebsite/knifeEdgeSpectral3D
 python gen_monitors.py
 source gen_movies.sh
-cd ~
-## Update Spectral 3D Pages
-cd public_html/resultsWebsite/spectral3D/
-cd public_html/resultsWebsite/knifeEdgeSpectral3D
+# Update Knife Edge Spectral Axisymmetric Pages
+cd ~/public_html/resultsWebsite/knifeEdgeSpectral3DAxisym
 python gen_monitors.py
 source gen_movies.sh
-cd ~
+# Update Free Surf Finite Difference Pages
+cd ~/public_html/resultsWebsite/freeSurfFD2D
+python gen_monitors.py
+source gen_movies.sh
 
 # Could be done more elegantly,
 # find public_html/Website/ -type f -iname "gen*" -exec bash {} \;
